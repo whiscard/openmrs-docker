c45215dc72bd86a7919eeddf476bace75309e253
Wes Brown <wes@weslandia.org>
2017-03-14 13:51:46 +0300

    Added docker-compose script and 1.11.6 platform.

--- run.sh
+++ run.sh
@@ -4,6 +4,22 @@
 if [ -d "/root/temp" ]; then
 # ------------ Begin Load Database ------------
 
+if [ -z ${OPENMRS_MYSQL_HOST+x} ]; then
+    OPENMRS_MYSQL_HOST=${MYSQL_PORT_3306_TCP_ADDR}
+fi
+echo "Using MySQL host: ${OPENMRS_MYSQL_HOST}"
+
+if [ -z ${OPENMRS_MYSQL_PORT+x} ]; then
+    OPENMRS_MYSQL_PORT=${MYSQL_PORT_3306_TCP_PORT}
+fi
+echo "Using MySQL port: ${OPENMRS_MYSQL_PORT}"
+
+# Ensure mysql is up
+while ! mysqladmin ping -h"$OPENMRS_MYSQL_HOST" -P $OPENMRS_MYSQL_PORT --silent; do
+    echo "Waiting for database at '$OPENMRS_MYSQL_HOST:$OPENMRS_MYSQL_PORT'..."
+    sleep 2
+done
+
 # Only set these variables if we're loading the demo data
 if [ -z ${DEMO_DATA+x} ]; then
     echo "Demo data will not be loaded (specify the DEMO_DATA parameter to load demo data).";
@@ -18,16 +34,6 @@ else
         echo "The mysql password parameter (DB_PASS) must be defined.";
         exit 1
     fi
-
-    if [ -z ${OPENMRS_MYSQL_HOST+x} ]; then
-        OPENMRS_MYSQL_HOST=${MYSQL_PORT_3306_TCP_ADDR}
-    fi
-    echo "Using MySQL host: ${OPENMRS_MYSQL_HOST}"
-
-    if [ -z ${OPENMRS_MYSQL_PORT+x} ]; then
-        OPENMRS_MYSQL_PORT=${MYSQL_PORT_3306_TCP_PORT}
-    fi
-    echo "Using MySQL port: ${OPENMRS_MYSQL_PORT}"
 
     if [ -z ${DB_NAME+x} ]; then
         DB_NAME=${DEFAULT_DB_NAME};
@@ -51,7 +57,7 @@ else
 
     # Check if the database already exists. If it does then do not create or import data but do ensure that the user has access
     if mysql -h $OPENMRS_MYSQL_HOST -P $OPENMRS_MYSQL_PORT -u $DB_USER --password=$DB_PASS -e "USE ${DB_NAME}"; then
-        echo "Database '${DB_NAME}' already exists. Database creation and import will not occur."
+        echo "Database '${DB_NAME}' already exists. Skipping database creation and import."
     else
         # Create database
         echo "Creating database..."
@@ -69,27 +75,26 @@ else
         do
             mysql -h $OPENMRS_MYSQL_HOST -P $OPENMRS_MYSQL_PORT -u $DB_USER --password=$DB_PASS ${DB_NAME}  < $script
         done
-
         rm /root/temp/db/*.sql
         echo "Demo data loaded."
 
         # ------------ Begin Load OpenHMIS Demo Data -----------------
 
-        if [ -z ${EXCLUDE_OPENHMIS+x} ]; then
-            echo "Loading OpenHMIS demo data..."
-            unzip -j ${OPENHMIS_LOCAL_DATABASE_SCRIPT_PATH} -d /root/temp/db/
-            SCRIPTS=/root/temp/db/*.sql
+       if [ -z ${EXCLUDE_OPENHMIS+x} ]; then
+           echo "Loading OpenHMIS demo data..."
+           unzip -j ${OPENHMIS_LOCAL_DATABASE_SCRIPT_PATH} -d /root/temp/db/
+           SCRIPTS=/root/temp/db/*.sql
 
-            for script in $SCRIPTS
-            do
-                mysql -h $OPENMRS_MYSQL_HOST -P $OPENMRS_MYSQL_PORT -u $DB_USER --password=$DB_PASS ${DB_NAME}  < $script
-            done
+           for script in $SCRIPTS
+           do
+               mysql -h $OPENMRS_MYSQL_HOST -P $OPENMRS_MYSQL_PORT -u $DB_USER --password=$DB_PASS ${DB_NAME}  < $script
+           done
 
-            rm /root/temp/db/*.sql
-            echo "Demo OpenHMIS data loaded."
-        fi
+           rm /root/temp/db/*.sql
+           echo "Demo OpenHMIS data loaded."
+       fi
 
-        # ------------ End Download OpenHMIS Modules -----------------
+       # ------------ End Download OpenHMIS Demo Data -----------------
     fi
 
     # Create OpenMRS db user
@@ -110,21 +115,26 @@ fi
 
 # ------------ End Load Database ------------
 
-# ------------ Begin Download OpenHMIS Modules -----------------
+# Copy base/dependency modules to module folder
+echo "Copying module dependencies and reference application modules..."
+mkdir -pv $OPENMRS_MODULES
+cp /root/temp/modules/*.omod $OPENMRS_MODULES
+echo "Modules copied."
 
+# ------------ Begin Download OpenHMIS Modules -----------------
 if [ -z ${EXCLUDE_OPENHMIS+x} ]; then
     echo "Downloading current OpenHMIS modules..."
 
     # Setup Variables
     DOWNLOAD_DIR=/root/temp/modules/openhmis
-    TEAMCITY_URL="http://build.openhmisafrica.org/teamcity"
+    TEAMCITY_URL="http://teamcity.openhmisafrica.org/"
     TEAMCITY_REST_ARTIFACT_URL="$TEAMCITY_URL/guestAuth/app/rest/builds/buildType:BUILD_TYPE/artifacts/children/"
     ARTIFACT_XPATH="string(/files/file/content/@href)"
 
     MODULE_PROJECT_NAMES=("commons_prod" "bbf_prod" "inv_prod" "cash_prod")
 
     # Clear openhmis module folder
-    mkdir -p ${DOWNLOAD_DIR}
+    mkdir -pv ${DOWNLOAD_DIR}
     rm ${DOWNLOAD_DIR}/*.omod
 
     # Get current OpenHMIS module assets from TeamCity (master)
@@ -147,14 +157,15 @@ if [ -z ${EXCLUDE_OPENHMIS+x} ]; then
 
     cp ${DOWNLOAD_DIR}/*.omod ${OPENMRS_MODULES}/
 
+    # ------------ End Download OpenHMIS Modules -----------------
 fi
-
-# ------------ End Download OpenHMIS Modules -----------------
 
 # Cleanup temp files
 rm -r /root/temp
-
 fi
+
+# Set custom memory options for tomcat
+export JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Xms256m -Xmx1024m -XX:PermSize=256m -XX:MaxPermSize=512m"
 
 # Run tomcat
 catalina.sh run
